03ecc928364ecd9a1be40770567bcb14ca81f8ad
hongsik2004 <66453621+hongsik2004@users.noreply.github.com>
2022-07-06 14:59:17 +0900

    2022-07-06(1차수정)

--- resoures/javascript/coin_noticce.js
+++ resoures/javascript/coin_noticce.js
@@ -2,3 +2,9 @@ $('.service-submenu a').click(function(){
         $('.service-submenu a').removeClass("on");
         $(this).addClass("on");
 });
+
+$('.pagination .num').click(function(){
+        $('.pagination .num').removeClass("action");
+        $(this).addClass("action");
+    });
+    
